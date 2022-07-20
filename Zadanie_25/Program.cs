251bc83a7f9357699bbfaf8b33007b7cbcdfe762
Andrey <acord.81@mail.ru>
2022-07-20 20:41:36 +0300

    Возводим в степень

---
+++ Zadanie_25/Program.cs
@@ -0,0 +1,16 @@
+﻿void Raise()
+{
+    Console.WriteLine("Введите число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int x1 = 1;
+
+    for (int i = 0; i < a; i++)
+    {
+        x1 = x1 * x;
+    }
+    Console.Write($"Число {x} в степени {a} равно {x1}");
+}
+
+Raise();
