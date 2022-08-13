f20adf5133b3f04a34209eec576cec251c59fbd2
Andrey <acord.81@mail.ru>
2022-08-13 16:14:51 +0300

    Создали программу подсчета положительных чисел в массиве

---
+++ Zadanie_41/Program.cs
@@ -0,0 +1,41 @@
+﻿// SЗадача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество чисел для анализа");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[x];
+
+FillArray(numbers);
+PrintArray(numbers);
+DetectNumbers(numbers);
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} - ое число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void DetectNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    Console.Write("Вы ввели " + count + " чисел больше 0");
+}
