5d4794af41a44da4bdf521099110a6073426c3ae
Andrey <acord.81@mail.ru>
2022-08-17 20:44:15 +0300

    создание двумерного массива из вещественных чисел размером m*n

---
+++ Zadanie_47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[m, n];
+Random random = new Random();
+
+FillArray(numbers, m, n);
+PrintArray(numbers, m, n);
+
+void FillArray(double[,] array, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(random.NextDouble() * 10 - 5, 1);
+        }
+    }
+}
+
+void PrintArray(double[,] array, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
