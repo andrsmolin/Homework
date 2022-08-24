7d3f97ce9a2b2ee6ea3d961d975978679e9eefef
Andrey <acord.81@mail.ru>
2022-08-24 21:05:11 +0300

    создали программу расчета суммы натуральных

---
+++ Zadanie_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n)
+{
+    int temp = m;
+    m = n;
+    n = temp;
+}
+Console.Write(SearchSumm(m, n));
+int SearchSumm(int a, int b)
+{
+    if (a == b) return a;
+    return a += SearchSumm(a + 1, b);
+}
