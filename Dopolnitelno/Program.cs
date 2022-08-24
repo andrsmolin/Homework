0eaf1fc8aa0bb6a310c6fa889e540ce84a6230a4
Andrey <acord.81@mail.ru>
2022-08-24 21:07:00 +0300

    задачи на семинаре

--- Dopolnitelno/Program.cs
+++ Dopolnitelno/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿void Rec(int number1, int number2)
+{
+    if (number2 < number1) return;
+
+    Console.WriteLine(number1);
+    number1++;
+    Rec(number1, number2);
+}
+Rec(4, 8);
+
+void Recur(int number1, int summ = 0)
+{
+    if (number1 == 0)
+    {
+        Console.Write(summ);
+        return;
+    }
+    else
+    {
+        summ += number1 % 10;
+        number1 = number1 / 10;
+    }
+    Recur(number1, summ);
+}
+
+Recur(88);
